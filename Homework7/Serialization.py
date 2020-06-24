d332028088880ffd93ccaa31a50e0d4e034c2a1f
Denis Ivanov <denisliv13@gmail.com>
2020-06-24 20:06:24 +0300

    Update

--- Homework7/Serialization.py
+++ Homework7/Serialization.py
@@ -28,7 +28,7 @@ class Integer(Field):
         return value.to_bytes(4, 'big')
 
     def load(self, data: bytes):
-        return int.from_bytes(data[0:4], 'big')
+        return int.from_bytes(data[0:4], 'big'), 4
 
     def convert(self, value):
         if isinstance(value, int):
@@ -51,11 +51,26 @@ class Float(Field):
 
     def dump(self, value):
         from struct import pack
-        return pack('>f', value)
+        return pack('>d', value)
 
     def load(self, data: bytes):
         from struct import unpack
-        return unpack('>f', data[4:])[0]
+        return unpack('>d', data[0:8])[0], 8
+
+class String(Field):
+    def convert(self, value):
+        if isinstance(value, str):
+            return value
+
+    def dump(self, value):
+        data = value.encode('utf-8')
+        n = len(data)
+        return n.to_bytes(2, 'big', signed=False) + data
+
+    def load(self, data):
+        n = int.from_bytes(data[0:2], 'big')
+        value = data[2:2+n].decode('utf-8')
+        return value, 2+n
 
 
 class Serializable(object):
@@ -73,10 +88,16 @@ class Serializable(object):
 
     @classmethod
     def load(cls, data: bytes) -> 'Test':
+        desc = {}
+        for base in reversed(cls.__mro__):
+            for key, value in base.__dict__.items():
+                if isinstance(value, Field):
+                    desc[key] = value
         x = {}
-        for key, value in cls.__dict__.items():
-            if isinstance(value, Field):
-                x[key] = value.load(data)
+        for key, field in desc.items():
+            value, n = field.load(data)
+            x[key] = value
+            data = data[n:]
         obj = Test.__new__(Test)
         obj.__dict__.update(x)
         return obj
@@ -85,10 +106,12 @@ class Serializable(object):
 class Test(Serializable):
     x = Integer()
     _y = Float()
+    __z = String()
 
-    def __init__(self, x: int, y: float):
+    def __init__(self, x: int, y: float, z):
         self.x: int = x
         self._y: float = y
+        self.__z: str = z
 
     def __eq__(self, other):
         if not isinstance(other, Test):
@@ -97,7 +120,7 @@ class Test(Serializable):
 
 
 x = 1
-a = Test(1, 2)
+a = Test(1, 2.69, "asdf")
 data = a.dump()
 print(data)
 b = Test.load(data)
